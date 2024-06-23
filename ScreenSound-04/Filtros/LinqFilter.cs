910c0805abbac5c7b906a0218fc4e296e1682859
Fabricio Felix <felixfabricio115@gmail.com>
2024-06-23 11:08:36 -0300

    filtrar musica de um artista

--- ScreenSound-04/Filtros/LinqFilter.cs
+++ ScreenSound-04/Filtros/LinqFilter.cs
@@ -18,8 +18,8 @@ internal class LinqFilter
 
     public static void FiltrarArtistasPorGeneroMusical(List<Musica> musicas, string genero)
     {
-        var artistasPorGeneroMusical = musicas.Where(musica => 
-        musica.Genero!.Contains(genero)).Select(musica => 
+        var artistasPorGeneroMusical = musicas.Where(musica =>
+        musica.Genero!.Contains(genero)).Select(musica =>
         musica.Artista).Distinct().ToList();
         Console.WriteLine($"exibindo artista por genero musical >>> {genero}");
         foreach (var artista in artistasPorGeneroMusical)
@@ -28,5 +28,16 @@ internal class LinqFilter
 
 
         }
+
+    }
+    public static void FiltrarMusicasDeUmArtista(List<Musica> musicas, string nomeDoArtista)
+    {
+        var musicasDoArtista = musicas.Where(musica => musica.Artista!.Equals(nomeDoArtista)).ToList();
+        Console.WriteLine(nomeDoArtista);
+        foreach (var musica in musicasDoArtista)
+        {
+            Console.WriteLine($"- {musica.Nome}");
+        }
+
     }
 }
